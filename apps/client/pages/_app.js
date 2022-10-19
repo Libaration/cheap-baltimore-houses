28179983a94323f7c62ca5f9bdbaa90e80bd9740
Cristian Deleon <libaration@gmail.com>
2022-10-19 19:46:24 -0400

    more unneeded parallax

--- apps/client/pages/_app.js
+++ apps/client/pages/_app.js
@@ -1,6 +1,14 @@
 import "../styles/global.css";
+import "primereact/resources/themes/rhea/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+import { ParallaxProvider } from "react-scroll-parallax";
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ParallaxProvider>
+      <Component {...pageProps} />
+    </ParallaxProvider>
+  );
 }
 
 export default MyApp;
--- apps/client/pages/index.js
+++ apps/client/pages/index.js
@@ -1,16 +1,52 @@
 import NavBar from "../components/NavBar";
+import { Button } from "primereact/button";
+import { useRef } from "react";
+import { useParallax } from "react-scroll-parallax";
 export default function Index(props) {
+  const page2Ref = useRef(null);
+  const target = useRef(null);
+  const text = useParallax({
+    speed: 10,
+    target: target.current,
+  });
+  const sky = useParallax({
+    opacity: [0, 1],
+    shouldAlwaysCompleteAnimation: true,
+    startScroll: 0,
+    endScroll: 200,
+  });
+  const city = useParallax({
+    opacity: [0, 1],
+    shouldAlwaysCompleteAnimation: true,
+    startScroll: 0,
+    endScroll: 200,
+  });
+
   return (
-    <div className="main">
-      <div className="backgroundHero">
-        <div className="text-center">
-          <h4 className="smallHeroText">Cheap Baltimore Houses </h4>
-          <h1 className="heroText">Cheap</h1>
-          <h1 className="heroText">Baltimore</h1>
-          <h1 className="heroText">Houses</h1>
+    <div className="main" ref={target}>
+      <img className="sky" src="/sky.png" alt="" ref={sky.ref} />
+      <img className="city" src="/city.png" alt="" ref={city.ref} />
+      <div className="section">
+        <div className="text-center" ref={text.ref} style={{ zIndex: 10 }}>
+          <h4 className="smallHeroText">Cheap Baltimore Houses</h4>
+          <h1 className="heroText">Buying property</h1>
+          <h1 className="heroText">has never been easier.</h1>
+          <div className="mt-5">
+            <Button
+              className="seeOurPropertiesButton"
+              label="See our properties"
+              onClick={() => {
+                page2Ref.current?.scrollIntoView({ behavior: "smooth" });
+              }}
+            />
+          </div>
         </div>
 
         <NavBar />
+      </div>
+
+      <div className="section" id="#next" ref={page2Ref}>
+        <h1>MORE STUFF I GUESS</h1>
       </div>
     </div>
   );
