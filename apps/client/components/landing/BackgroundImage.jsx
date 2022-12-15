a0775cdae2c29f0ee621a8737684d469ac2ebf54
Cristian Deleon <libaration@gmail.com>
2022-12-15 00:07:19 -0500

    component for bg image

---
+++ apps/client/components/landing/BackgroundImage.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const BackgroundImage = () => {
+  return (
+    <div
+      className="bgfade"
+      style={{
+        backgroundImage:
+          "url(https://images.unsplash.com/photo-1617957718614-8c23f060c2d0?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=3432&q=80)",
+
+        backgroundRepeat: "no-repeat",
+
+        position: "fixed",
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        zIndex: -1,
+      }}
+    ></div>
+  );
+};
+
+export default BackgroundImage;
