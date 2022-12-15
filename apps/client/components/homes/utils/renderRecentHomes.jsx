59701149f991bd50aec08dc30f145dd40b5a6ba3
Cristian Deleon <libaration@gmail.com>
2022-12-15 00:07:05 -0500

    refactor util function for readability

--- apps/client/components/homes/utils/renderRecentHomes.jsx
+++ apps/client/components/homes/utils/renderRecentHomes.jsx
@@ -1,11 +1,13 @@
 import HomeCard from "../HomeCard";
 
+const renderHomeCard = (home) => {
+  return (
+    <div key={`HOME_${home.id}`}>
+      <HomeCard home={home} />
+    </div>
+  );
+};
+
 export const renderRecentHomes = (homes) => {
-  return homes.map((home) => {
-    return (
-      <div key={`HOME_${home.id}`}>
-        <HomeCard home={home} />
-      </div>
-    );
-  });
+  return homes.map(renderHomeCard);
 };
