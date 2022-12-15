de1d07efa79f5de9aea69399e53ec211e734e015
Cristian Deleon <libaration@gmail.com>
2022-12-15 00:08:19 -0500

    readability

--- apps/client/lib/homes.js
+++ apps/client/lib/homes.js
@@ -1,6 +1,7 @@
 import { fetchAPI } from "./api";
-const recentHomes = async (limit) =>
-  await fetchAPI(
+const recentHomes = async (limit) => {
+  return await fetchAPI(
     `/homes?populate=*&pagination[pageSize]=${limit}&sort=id:DESC`
   );
+};
 export const homesCalls = { get: { recentHomes } };
