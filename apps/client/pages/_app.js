cf295d156f5514911d7b96476f8195a86badf48c
Cristian Deleon <libaration@gmail.com>
2022-12-15 21:22:22 -0500

    mobile device fixes

--- apps/client/pages/_app.js
+++ apps/client/pages/_app.js
@@ -5,12 +5,38 @@ import "primeicons/primeicons.css";
 import { ParallaxProvider } from "react-scroll-parallax";
 import { builder } from "@builder.io/react";
 import "@builder.io/widgets/dist/lib/builder-widgets-async";
+import Head from "next/head";
 builder.init(process.env.NEXT_PUBLIC_BUILDER_KEY);
+import { useEffect } from "react";
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    const handleResize = () => {
+      let vh = window.innerHeight;
+
+      document.documentElement.style.setProperty("--app-height", `${vh}px`);
+    };
+    handleResize();
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
   return (
-    <ParallaxProvider>
-      <Component {...pageProps} />
-    </ParallaxProvider>
+    <>
+      <Head>
+        <meta
+          name="viewport"
+          content="viewport-fit=cover, width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
+        />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta
+          name="apple-mobile-web-app-status-bar-style"
+          content="black-translucent"
+        />
+        <meta name="theme-color" content="#161724" />
+      </Head>
+      <ParallaxProvider>
+        <Component {...pageProps} />
+      </ParallaxProvider>
+    </>
   );
 }
 
