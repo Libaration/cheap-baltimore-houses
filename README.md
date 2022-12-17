e80c8e20e6932b2ea46bc377b0429bf6b4780e67
MBRound18 <12646562+mbround18@users.noreply.github.com>
2022-12-16 22:39:53 -0800

    Im Helping :D

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cheap Baltimore Houses
+
+(Cheap is a relative term)
+
+## Getting Started
+
+```sh
+git clone git@github.com:Libaration/cheap-baltimore-houses.git
+
+cd cheap-baltimore-houses
+yarn install
+```
--- apps/cheap-baltimore-houses/src/api/mailing/services/mailing.js
+++ apps/cheap-baltimore-houses/src/api/mailing/services/mailing.js
@@ -1,9 +1,9 @@
-'use strict';
+"use strict";
 
 /**
  * mailing service
  */
 
-const { createCoreService } = require('@strapi/strapi').factories;
+const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService('api::mailing.mailing');
+module.exports = createCoreService("api::mailing.mailing");
