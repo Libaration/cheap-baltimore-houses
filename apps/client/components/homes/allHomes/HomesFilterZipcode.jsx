3a5e3256aaf0247b9b91b6efc39d241e00215a91
Cristian Deleon <libaration@gmail.com>
2023-01-05 17:07:28 -0500

    fix debouncing

--- apps/client/components/homes/allHomes/HomesFilterZipcode.jsx
+++ apps/client/components/homes/allHomes/HomesFilterZipcode.jsx
@@ -1,23 +1,35 @@
 import { Input } from "@nextui-org/react";
 import { debounce } from "lodash";
 import { useState } from "react";
+import { useEffect, useMemo } from "react";
 const HomesFilterZipcode = ({ setShouldFetchByZipcode, setZipcode }) => {
-  const [textboxZipcode, setTextboxZipcode] = useState("");
+  const debouncedSetZipcode = useMemo(() => {
+    return debounce(setZipcode, 1500);
+  }, []);
+  useEffect(() => {
+    return () => {
+      debouncedSetZipcode.cancel();
+    };
+  }, []);
   const handleChange = (e) => {
-    if (textboxZipcode !== "") {
-      setShouldFetchByZipcode(true);
-      const debouncedSetZipcode = debounce(setZipcode, 1500);
+    console.log(e.target.value);
+    if (
+      e.target.value !== "" ||
+      e.target.value !== " " ||
+      e.target.value !== null ||
+      e.target.value !== undefined
+    ) {
+      e.target.value = e.target.value.replace(/[^0-9]/g, "");
       debouncedSetZipcode(e.target.value);
+      setShouldFetchByZipcode(true);
     } else {
       setShouldFetchByZipcode(false);
     }
-    setTextboxZipcode(e.target.value);
   };
   return (
     <Input
       placeholder="Zipcode"
       className="mt-5 mb-5"
-      value={textboxZipcode}
       aria-label="Zipcode"
       onChange={handleChange}
     />
