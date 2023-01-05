a98c07fc7028b55b849408ddc34f65b2c79c433a
Cristian Deleon <libaration@gmail.com>
2023-01-05 15:59:05 -0500

    adds gallery/lightbox to individual home route

--- apps/client/pages/homes/[id].jsx
+++ apps/client/pages/homes/[id].jsx
@@ -1,20 +1,84 @@
 import { homesCalls } from "../../lib/homes";
-import Image from "next/future/image";
-
-import { cloudinaryLoader } from "../../lib/cloudinaryLoader";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import { generateMarkdown } from "../../lib/markDownMaker";
 import Breadcrumb from "../../components/Breadcrumb";
 import Head from "next/head";
-
+import PhotoAlbum from "react-photo-album";
+import NextJsImage from "../../components/homes/NextJsImage";
+import Image from "next/future/image";
+import Lightbox from "yet-another-react-lightbox";
+import Fullscreen from "yet-another-react-lightbox/plugins/fullscreen";
+import "yet-another-react-lightbox/styles.css";
+import "yet-another-react-lightbox/plugins/thumbnails.css";
+import { cloudinaryLoader } from "../../lib/cloudinaryLoader";
+import Thumbnails from "yet-another-react-lightbox/plugins/thumbnails";
+import Zoom from "yet-another-react-lightbox/plugins/zoom";
 const HomeShow = ({ home }) => {
+  const [index, setIndex] = useState(-1);
+  const Gallery = useCallback(
+    ({ view, photos }) => (
+      <PhotoAlbum
+        layout={view}
+        photos={photos}
+        renderPhoto={NextJsImage}
+        onClick={({ index }) => setIndex(index)}
+      />
+    ),
+    []
+  );
   const coverImage =
     home.attributes.cover_image.data.attributes.provider_metadata.public_id;
+  const coverImageHeight = home.attributes.cover_image.data.attributes.height;
+  const coverImageWidth = home.attributes.cover_image.data.attributes.width;
+  const additionalImages =
+    home.attributes.additional_images.data &&
+    home.attributes.additional_images.data.map((image, index) => {
+      return {
+        src: image.attributes.provider_metadata.public_id,
+        width: image.attributes.width,
+        height: image.attributes.height,
+        alt: `Home Additional Image ${index}`,
+      };
+    });
+  const photos = [
+    {
+      src: coverImage,
+      width: coverImageWidth,
+      height: coverImageHeight,
+      alt: "Home Cover Image",
+    },
+    ...(additionalImages || []),
+  ];
+  const slides = photos.map(({ src, alt, width, height }) => ({
+    src: cloudinaryLoader({ src, width }),
+    alt,
+    width,
+    height,
+    srcSet: [
+      { src: cloudinaryLoader({ src, width: 320 }), width: 320, height: 213 },
+      { src: cloudinaryLoader({ src, width: 640 }), width: 640, height: 427 },
+      { src: cloudinaryLoader({ src, width: 1200 }), width: 1200, height: 800 },
+      {
+        src: cloudinaryLoader({ src, width: 2048 }),
+        width: 2048,
+        height: 1365,
+      },
+      {
+        src: cloudinaryLoader({ src, width: 3840 }),
+        width: 3840,
+        height: 2560,
+      },
+    ],
+  }));
+  const [view, setView] = useState(additionalImages ? "columns" : "rows");
   const date = new Date(home.attributes.createdAt).toLocaleDateString("en-US", {
     year: "numeric",
     month: "long",
     day: "numeric",
   });
+  const handleViewChange = (e) => {
+    setView(e.target.name);
+  };
   const address = `${home.attributes.street} ${
     home.attributes.street2 ? home.attributes.street2 : ""
   } ${home.attributes.city}, ${home.attributes.state} ${home.attributes.zip}`;
@@ -34,19 +98,66 @@ const HomeShow = ({ home }) => {
       <h1 id="small-address-hero-text" className="text-center">
         {address}
       </h1>
-      <div className="flex items-center justify-center">
-        <div
-          className="text-black h-full p-5 max-w-full"
-          style={{ width: "40em" }}
-        >
+      <div className="flex justify-center">
+        <ol>
+          <li className="float-left mr-2 ml-2">
+            <Image
+              src="/column.png"
+              height="24"
+              width="24"
+              alt="column-view"
+              name="columns"
+              className="object-cover cursor-pointer"
+              onClick={handleViewChange}
+            />
+          </li>
+          <li className="float-left ml-2 mr-2">
+            <Image
+              src="/row.png"
+              height="24"
+              width="24"
+              alt="row-view"
+              name="rows"
+              className="object-cover cursor-pointer"
+              onClick={handleViewChange}
+            />
+          </li>
+          <li className="float-left mr-2 ml-2">
+            <Image
+              src="/masonry.png"
+              height="24"
+              width="24"
+              alt="masonry-view"
+              name="masonry"
+              className="object-cover cursor-pointer"
+              onClick={handleViewChange}
+            />
+          </li>
+        </ol>
+      </div>
+
+      <div className="flex items-center justify-center ">
+        <div className="text-black h-full p-5 max-w-full">
           <div className="mx-auto overflow-hidden rounded-lg bg-white shadow">
-            <div className="relative aspect-video">
-              <Image
-                loader={cloudinaryLoader}
-                src={`${coverImage}`}
-                fill
-                alt="home"
-                className="object-cover"
+            <div className="relative">
+              <Gallery view={view} photos={photos} />
+              <Lightbox
+                slides={slides}
+                open={index >= 0}
+                index={index}
+                close={() => setIndex(-1)}
+                inline={{
+                  style: {
+                    width: "100%",
+                    maxWidth: "900px",
+                    aspectRatio: "3 / 2",
+                  },
+                }}
+                styles={{
+                  thumbnail: { border: "none" },
+                  root: { "--yarl__color_backdrop": "rgba(0, 0, 0, .9)" },
+                }}
+                plugins={[Fullscreen, Thumbnails, Zoom]}
               />
             </div>
             <div className="p-4">
@@ -92,6 +203,6 @@ export async function getStaticPaths() {
 export async function getStaticProps(context) {
   const res = await homesCalls.get.getHome(context.params.id);
   const home = res.data;
-  return { props: { home } };
+  return { props: { home }, revalidate: 300 };
 }
 export default HomeShow;
