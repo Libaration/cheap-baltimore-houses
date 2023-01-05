f125cd214c8222c54752a59c11dcc99715ede505
Cristian Deleon <libaration@gmail.com>
2023-01-05 15:58:00 -0500

    console.log cleanup

--- apps/client/components/homes/HomesPagination.jsx
+++ apps/client/components/homes/HomesPagination.jsx
@@ -2,7 +2,6 @@ import { Pagination } from "@nextui-org/react";
 import { homesCalls } from "../../lib/homes";
 import { useSWRAllHomesPaginated } from "../../lib/homes";
 const HomesPagination = ({ meta, setHomes, setMeta }) => {
-  console.log(meta);
   return (
     <div className="flex items-center mt-5 mb-5 flex-col">
       <Pagination
