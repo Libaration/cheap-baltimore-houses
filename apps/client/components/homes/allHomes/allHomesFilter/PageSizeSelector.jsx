0bf6f4671b93df1060446ede1600a6b61a8595f9
Cristian Deleon <libaration@gmail.com>
2023-01-08 16:28:59 -0500

    PageSize selector

--- apps/client/components/homes/allHomes/allHomesFilter/PageSizeSelector.jsx
+++ apps/client/components/homes/allHomes/allHomesFilter/PageSizeSelector.jsx
@@ -1,31 +1,36 @@
-const PageSizeDropdown = () => {
+import { useEffect } from "react";
+const PageSizeSelector = ({ setPage, setPageSize, pageSize }) => {
+  const onPageSizeChange = (eventChange) => {
+    setPage(1);
+    setPageSize(parseInt(eventChange.target.innerText));
+  };
   return (
     <>
       <div className="flex flex-row">
         <button
           type="button"
           className="rounded-lg border border-primary-500 bg-primary-500 px-3 py-1.5 text-center text-xs font-medium text-white shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300 ml-2 mr-2"
-          onClick={() => console.log("")}
+          onClick={onPageSizeChange}
         >
           3
         </button>
         <button
           type="button"
           className="rounded-lg border border-primary-500 bg-primary-500 px-3 py-1.5 text-center text-xs font-medium text-white shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300 ml-2 mr-2"
-          onClick={() => console.log("")}
+          onClick={onPageSizeChange}
         >
           6
         </button>
         <button
           type="button"
           className="rounded-lg border border-primary-500 bg-primary-500 px-3 py-1.5 text-center text-xs font-medium text-white shadow-sm transition-all hover:border-primary-700 hover:bg-primary-700 focus:ring focus:ring-primary-200 disabled:cursor-not-allowed disabled:border-primary-300 disabled:bg-primary-300 ml-2 mr-2"
-          onClick={() => console.log("")}
+          onClick={onPageSizeChange}
         >
           9
         </button>
       </div>
-      <span className="filter-spans inline-block mt-2">9 Per Page</span>
+      <span className="filter-spans inline-block mt-2">{pageSize} Per Page</span>
     </>
   );
 };
-export default PageSizeDropdown;
+export default PageSizeSelector;
