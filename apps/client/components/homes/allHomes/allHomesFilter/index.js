0df13a457a3d88c1ab3f85b68c1e645350858497
Cristian Deleon <libaration@gmail.com>
2023-01-08 16:28:31 -0500

    rename dropdown add pagination

--- apps/client/components/homes/allHomes/allHomesFilter/index.js
+++ apps/client/components/homes/allHomes/allHomesFilter/index.js
@@ -1,3 +1,4 @@
 export { default as Accordion } from "./Accordion";
 export { default as FilterZipcodeInput } from "./FilterZipcodeInput";
-export { default as PageSizeDropdown } from "./PagesizeDropdown";
+export { default as PagePagination } from "./PagePagination";
+export { default as PageSizeSelector } from "./PageSizeSelector";
