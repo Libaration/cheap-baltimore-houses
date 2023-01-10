ca55de7b1ef6eb1ccf03970620df981dba5b0a49
Cristian Deleon <libaration@gmail.com>
2023-01-09 21:44:03 -0500

    fix refetching on emails / revalidation

--- apps/client/components/newsletter/SignUpForm.jsx
+++ apps/client/components/newsletter/SignUpForm.jsx
@@ -1,17 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { useCheckEmail } from "../../lib/SWRCalls/user";
 import { Button, Input, useInput } from "@nextui-org/react";
 const SignUpForm = (props) => {
   const { value, reset, bindings } = useInput("");
   const [shouldCheck, setShouldCheck] = useState(false);
   const [isDisabled, setIsDisabled] = useState("disabled");
+  const [isValid, setIsValid] = useState(false);
 
-  const { data, error, isLoading } = useCheckEmail(shouldCheck, value);
-
-  //   const handleSubmit = (event) => {
-  //     event.preventDefault();
-  //     onSubmit({ email, password });
-  //   };
+  const { data, isLoading } = useCheckEmail(shouldCheck, value);
   const validateEmail = (value) => {
     const isValidEmail = value.match(
       /(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/i
@@ -19,10 +15,6 @@ const SignUpForm = (props) => {
     setIsDisabled(!isValidEmail);
     return isValidEmail;
   };
-  const handleButtonClick = () => {
-    setShouldCheck(true);
-  };
-
   const helper = React.useMemo(() => {
     if (!value) {
       setIsDisabled("disabled");
@@ -31,7 +23,7 @@ const SignUpForm = (props) => {
         color: "",
       };
     }
-    const isValid = validateEmail(value);
+    setIsValid(validateEmail(value));
     if (isValid) {
       return {
         text: "Correct email",
@@ -49,8 +41,23 @@ const SignUpForm = (props) => {
         color: "warning",
       };
     }
-  }, [isLoading, value]);
-
+  }, [value, isLoading]);
+  useEffect(() => {
+    if (data && data.disposable) {
+      setIsValid(false);
+      helper.color = "error";
+      helper.text = "Disposable emails are not allowed";
+      setShouldCheck(false);
+    } else if (data && !data.disposable) {
+      setIsValid(true);
+      helper.color = "success";
+      helper.text = "Correct email";
+      setShouldCheck(false);
+    }
+  }, [data, helper]);
+  const handleButtonClick = () => {
+    setShouldCheck(true);
+  };
   return (
     <div className="mx-auto relative max-w-xl">
       <div className="mb-10">
@@ -74,7 +81,7 @@ const SignUpForm = (props) => {
       </div>
       <span className="text-sm">or</span> <br />
       <div className="flex justify-center">
-        <Button color="success" size="md" disabled={isDisabled}>
+        <Button color="success" size="md" onClick={handleButtonClick} disabled={isDisabled}>
           Create an Account
         </Button>
       </div>
--- apps/client/lib/SWRCalls/user.js
+++ apps/client/lib/SWRCalls/user.js
@@ -1,9 +1,16 @@
 import useSWR from "swr";
+
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
 export function useCheckEmail(shouldCheck, email) {
   const { data, error, isLoading } = useSWR(
     shouldCheck ? `https://open.kickbox.com/v1/disposable/${email}` : null,
-    fetcher
+    fetcher,
+    {
+      revalidateOnFocus: false,
+      revalidateIfStale: false,
+      revalidateOnReconnect: false,
+      dedupingInterval: 12000,
+    }
   );
   return {
     data,
