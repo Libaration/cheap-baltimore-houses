b442bfa67ef7b62b85ef8371f619417bacdfb77c
Cristian Deleon <libaration@gmail.com>
2023-01-10 14:22:14 -0500

    useUser hook for getting profile info

--- apps/client/lib/SWRCalls/user.js
+++ apps/client/lib/SWRCalls/user.js
@@ -1,6 +1,31 @@
 import useSWR from "swr";
 import { getStrapiURL } from "../api";
+const getTokenFromStorage = () => {
+  if (typeof window !== "undefined") {
+    window.localStorage.getItem("token");
+  } else {
+    return null;
+  }
+};
 const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const authorizedFetcher =
+  (token) =>
+  (...args) =>
+    fetch(...args, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }).then((res) => {
+      if (!res.ok) {
+        const error = new Error("An error occurred while fetching the data.");
+        error.info = res.json();
+        error.status = res.status;
+        throw error;
+      } else {
+        return res.json();
+      }
+    });
+
 export function useCheckEmail(shouldCheck, email) {
   const { data, error, isLoading } = useSWR(
     shouldCheck ? `https://open.kickbox.com/v1/disposable/${email}` : null,
@@ -14,6 +39,18 @@ export function useCheckEmail(shouldCheck, email) {
   );
   return {
     data,
+    isLoading,
+    isError: error,
+  };
+}
+
+export function useUser(token) {
+  const { data, error, isLoading } = useSWR(
+    getStrapiURL("/api/users/me"),
+    authorizedFetcher(token)
+  );
+  return {
+    user: data,
     isLoading,
     isError: error,
   };
