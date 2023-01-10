54db8ff5235ceb5435522db485ee656e9086c1c0
Cristian Deleon <libaration@gmail.com>
2023-01-10 11:03:53 -0500

    add inputs for password&email / use score to determine button state

--- apps/client/components/user/UserRegister.jsx
+++ apps/client/components/user/UserRegister.jsx
@@ -1,10 +1,13 @@
-import { Container, Card, Row, Text, Button, Input } from "@nextui-org/react";
+import { Container, Card, Row, Text, Button, Input, Spacer } from "@nextui-org/react";
 import Lottie from "react-lottie-player";
 import { useState } from "react";
 import { useRouter } from "next/router";
+import { checkPasswordStrength } from "./utils/passwordStrength";
 const UserRegister = ({ animationData }) => {
   const router = useRouter();
   const [isPlaying, setIsPlaying] = useState(true);
+  const [password, setPassword] = useState("");
+  const { passwordLabel, inputColor, score } = checkPasswordStrength(password);
   const [endTime, setEndTime] = useState(37.4985);
   const handleRegisterClick = () => {
     setEndTime(141);
@@ -34,10 +37,29 @@ const UserRegister = ({ animationData }) => {
 
           <Row justify="center" align="center">
             <Text size={15} css={{ m: 0 }}></Text>
-            <Input value={router.query.email || ""} />
+            <Input
+              value={router.query.email || ""}
+              readOnly
+              label="Email"
+              size="sm"
+              status="success"
+              rounded
+              width="10em"
+            />
+          </Row>
+          <Spacer y={0.5} />
+          <Row justify="center" align="center">
+            <Input.Password
+              label={passwordLabel}
+              initialValue={password}
+              size="sm"
+              color={inputColor}
+              rounded
+              onChange={(e) => setPassword(e.target.value)}
+            />
           </Row>
           <Row justify="center" align="center" className="mt-5">
-            <Button onPress={handleRegisterClick} color="warning">
+            <Button onPress={handleRegisterClick} color="warning" disabled={score < 2}>
               Register
             </Button>
           </Row>
