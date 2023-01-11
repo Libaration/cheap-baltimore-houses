ef1b400d28f93131c4cd533b4382396c225180d9
Cristian Deleon <libaration@gmail.com>
2023-01-11 12:29:20 -0500

    create logout function

--- apps/client/lib/SWRCalls/session.js
+++ apps/client/lib/SWRCalls/session.js
@@ -1,4 +1,4 @@
-import { getCookie, setCookie } from "cookies-next";
+import { getCookie, setCookie, deleteCookie } from "cookies-next";
 
 export const isLoggedIn = ({ req = "", res = "" }) => {
   const token = getCookie("token", { req, res });
@@ -11,4 +11,8 @@ export const loginWithTokenOrUser = async ({ jwt, user: { username, email } }) =
   } else {
     console.log("logging in with user");
   }
+};
+
+export const logout = ({ req = "", res = "" }) => {
+  deleteCookie("token", { req, res });
 };
