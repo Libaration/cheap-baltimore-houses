31ff4be7286c10cafc90c25b3c3c4eb895d9c904
Cristian Deleon <libaration@gmail.com>
2023-01-11 12:05:04 -0500

    more error  details plz

--- apps/client/lib/SWRCalls/config.js
+++ apps/client/lib/SWRCalls/config.js
@@ -13,5 +13,12 @@ export const fetcherWithAuth = async (...args) => {
   const response = await fetch(...args, {
     headers: getAuthorizedHeaders({}),
   });
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    // Attach extra info to the error object.
+    error.info = await response.json();
+    error.status = response.status;
+    throw error;
+  }
   return response.json();
 };
