310a5530a9839efad8ce73fb34bf6156a3af0fef
Cristian Deleon <libaration@gmail.com>
2023-02-09 06:48:49 -0500

    hide creation

--- apps/client/components/newsletter/SignUpForm.jsx
+++ apps/client/components/newsletter/SignUpForm.jsx
@@ -82,12 +82,12 @@ const SignUpForm = () => {
           Sign Up
         </Button>
       </div>
-      <span className="text-sm">or</span> <br />
+      {/* <span className="text-sm">or</span> <br />
       <div className="flex justify-center">
         <Button color="success" size="md" onClick={handleButtonClick} disabled={isDisabled}>
           Create an Account
         </Button>
-      </div>
+      </div> */}
     </div>
   );
 };
