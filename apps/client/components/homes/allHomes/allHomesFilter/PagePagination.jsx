0473cb1bcf6666830d44a37fd0197aadb6b91db4
Cristian Deleon <libaration@gmail.com>
2023-05-26 18:21:17 -0400

    Update PagePagination component styles and color scheme

--- apps/client/components/homes/allHomes/allHomesFilter/PagePagination.jsx
+++ apps/client/components/homes/allHomes/allHomesFilter/PagePagination.jsx
@@ -5,14 +5,14 @@ const PagePagination = ({ meta, setPage, page }) => {
       <Pagination
         total={meta.pagination.pageCount} // confusing variable names here but in this case total is the total number of pages aka pageCount
         page={page}
-        color="warning"
-        size="sm"
+        size="xs"
+        color={"gradient"}
         controls={false}
         onChange={(chosenPage) => {
           setPage(chosenPage);
         }}
       />
-      <span className="text-xs mt-1 text-secondary-800">
+      <span className="text-xs mt-1 " style={{ color: "#333333" }}>
         Page {meta.pagination.page} of {meta.pagination.pageCount}
       </span>
     </div>
