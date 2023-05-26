1e1d600b85ab810bb3a9b4853d00ad11b6a987d0
Cristian Deleon <libaration@gmail.com>
2023-05-26 18:21:00 -0400

    ✨ feat: Add fade-in animation for all homes filter accordion
    The Accordion component in `apps/client/components/homes/allHomes/allHomesFilter/Accordion.jsx` now uses the `Fade` component from `react-awesome-reveal` to add a smooth fade-in animation when the filters are displayed. The background color of the details element was also changed to `#333333`.

--- apps/client/components/homes/allHomes/allHomesFilter/Accordion.jsx
+++ apps/client/components/homes/allHomes/allHomesFilter/Accordion.jsx
@@ -1,10 +1,11 @@
+import { Fade } from "react-awesome-reveal";
 const Accordion = ({ children }) => {
   return (
     <div className="flex justify-center mt-10">
-      <details className="group rounded-lg p-6 open:bg-gray-700 w-96 bg-gray-700">
+      <details className="group rounded-lg p-6 w-96 " style={{ backgroundColor: "#333333" }}>
         <summary className="flex cursor-pointer list-none items-center justify-between text-lg font-medium text-secondary-100">
           Filters
-          <div className="text-primary-500">
+          <div style={{ color: "#00b0ff" }}>
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -35,7 +36,10 @@ const Accordion = ({ children }) => {
             </svg>
           </div>
         </summary>
-        <div className="mt-2 text-secondary-200 text-xs">{children}</div>
+
+        <Fade cascade>
+          <div className="mt-2 text-secondary-200 text-xs">{children}</div>
+        </Fade>
       </details>
     </div>
   );
