c2f5a030cbd82d96ca35765201409f1eee68d1da
Cristian Deleon <libaration@gmail.com>
2023-05-26 18:22:37 -0400

    Refactor HomeSection and Index components for better UI

--- apps/client/components/landing/HomeSection.jsx
+++ apps/client/components/landing/HomeSection.jsx
@@ -1,14 +1,13 @@
 import Image from "next/future/image";
 import { Slide } from "react-awesome-reveal";
-import { useParallax } from "react-scroll-parallax";
-import { useRef } from "react";
+import Link from "next/link";
 const HomeSection = () => {
   return (
     <>
       <section className="text-gray-700 body-font landing-home-section">
         <div className="container mx-auto flex pb-20 md:flex-row flex-col items-center">
-          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white mt-4">
+          <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center center-safe">
+            <h1 className="title-font sm:text-4xl text-3xl mb-4 font-medium text-white mt-4 ">
               Buy and Sell Real Estate in Maryland.
               <br className="hidden lg:inline-block" />
               Find Cheap Baltimore Houses.
@@ -19,12 +18,12 @@ const HomeSection = () => {
               Cheap Baltimore Houses. Start your journey.
             </p>
 
-            <div className="flex justify-center">
-              <button className="inline-flex text-grey bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-[#00b0ff] duration-300 rounded text-lg">
-                I Want to Buy
+            <div className="flex justify-center center-safe">
+              <button className="inline-flex text-grey bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-[#333333] duration-300 rounded text-lg">
+                <Link href={"/homes"}> I Want to Buy </Link>
               </button>
-              <button className="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-[#00b0ff] duration-300 rounded text-lg">
-                I Want to Sell
+              <button className="ml-4 inline-flex text-gray-700 bg-gray-200 border-0 py-2 px-6 focus:outline-none hover:bg-[#333333] duration-300 rounded text-lg">
+                <Link href={"#"}> I Want to Sell </Link>
               </button>
             </div>
           </div>
